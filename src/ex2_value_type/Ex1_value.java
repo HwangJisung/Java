2d5977fcf87459282254f657d7ac479bef509b35
pc <pc@DESKTOP-DK0PFDP>
2019-09-06 21:17:10 +0900

    ex2_finish

--- src/ex2_value_type/Ex1_value.java
+++ src/ex2_value_type/Ex1_value.java
@@ -1,5 +1,17 @@
 package ex2_value_type;
 
 public class Ex1_value {
-
+	public static void main(String[] args) {
+		// 기본자료형(valueType)
+		// 논리형 : boolean
+		// 문자형 : char  
+		// 정수형 : byte(1), short(2), int(4), long(8) 
+        // 실수형 : float(4), double(8)
+		int a = 100; 
+		int b = 200;
+		
+		int c = a+b;
+		System.out.println(c);
+			
+	}
 }
