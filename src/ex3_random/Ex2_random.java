5a7609711814f29f5dfce8ddd0a998161f03b746
pc <pc@DESKTOP-DK0PFDP>
2019-09-20 21:19:10 +0900

    라인 정리 추가

--- src/ex3_random/Ex2_random.java
+++ src/ex3_random/Ex2_random.java
@@ -6,16 +6,14 @@ public class Ex2_random {
 	public static void main(String[] args) {
 		// 1 ~ 10 사이의 난수를 발생하여 구구단을 출력
 		// 단, 1이나 10이 발생됐을 경우에는 "잘못되었습니다"라는 문장을 출력
-		
+
 		int rnd = new Random().nextInt(9) + 1;
 
-		if(rnd == 1 || rnd == 10)
+		if (rnd == 1 || rnd == 10)
 			System.out.println(rnd + "은 잘못되었습니다.");
-		else
-		{
-			for(int i = 1; i<10; i++)
-			{
-				System.out.printf("%d * %d = %d\n",rnd,i,rnd*i);
+		else {
+			for (int i = 1; i < 10; i++) {
+				System.out.printf("%d * %d = %d\n", rnd, i, rnd * i);
 			}
 		}
 	}
