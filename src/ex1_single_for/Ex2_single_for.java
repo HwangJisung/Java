5903f27d3080853932d9da5d23ff55ec01ec3898
pc <pc@DESKTOP-DK0PFDP>
2019-09-25 19:53:13 +0900

    왜 한글 깨질까

--- src/ex1_single_for/Ex2_single_for.java
+++ src/ex1_single_for/Ex2_single_for.java
@@ -2,7 +2,7 @@ package ex1_single_for;
 
 public class Ex2_single_for {
 	public static void main(String[] args) {
-		// d변수에 해당되는 구구단을 출력하세요
+		// d변수에 해당되는 구구단을 출력하세요 
 		// 6*1 = 6
 		int d = 6;
 		
