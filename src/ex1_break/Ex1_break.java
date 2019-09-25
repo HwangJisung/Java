30d077793fb92b1ee83244bbd6f6908a5a303cad
pc <pc@DESKTOP-DK0PFDP>
2019-09-25 19:31:26 +0900

    check github

---
+++ src/ex1_break/Ex1_break.java
@@ -0,0 +1,11 @@
+package ex1_break;
+
+public class Ex1_break {
+	public static void main(String[] args) {
+		//break문 : 반복문 내에서 강제적으로 가장 가까운 반복문을 빠져나가기 위해 사용하는 키워드
+		
+		for(int i=0; i <=5; i++) {
+			System.out.println(i);
+		}
+	}
+}
