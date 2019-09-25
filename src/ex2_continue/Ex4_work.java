564c2dd779f485628f54736cec1720edaf2c29ae
pc <pc@DESKTOP-DK0PFDP>
2019-09-25 21:02:52 +0900

    work finish

--- src/ex2_continue/Ex4_work.java
+++ src/ex2_continue/Ex4_work.java
@@ -4,9 +4,9 @@ public class Ex4_work {
 	public static void main(String[] args) {
 		// 1~ 1000까지의 정수 중 3의 배수끼리만 더한 값을 출력하시오
 		// 결과 : 166833
-		
 		int i = 1;
 		int sum = 0;
+		
 		while ( i <= 1000)
 		{
 			if( i % 3 == 0)
